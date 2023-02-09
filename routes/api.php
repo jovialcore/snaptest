cb89a7f9c860645a46f5239b5d60ac48229cb67c
Chidiebere Chukwudi <jollydosky@gmail.com>
2023-02-09 13:09:34 +0100

    intiating api controller

--- routes/api.php
+++ routes/api.php
@@ -3,6 +3,8 @@
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
+
+use App\Http\Controllers\API\CitizensController;
 /*
 |--------------------------------------------------------------------------
 | API Routes
@@ -14,6 +16,11 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
-    return $request->user();
+// Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
+//     return $request->user();
+// });
+
+
+Route::middleware('auth:sanctum')->group(function () {
+    Route::resource('citizens', CitizensController::class);
 });
